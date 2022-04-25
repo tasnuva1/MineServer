729ade357648031694961266b833b09345621fbc
tasnuva1 <fahmidamashura@gmail.com>
2022-04-26 02:04:32 +0600

    New Version

---
+++ run.js
@@ -0,0 +1,19 @@
+const mineflayer = require("mineflayer");
+var options = {
+  host: "MadeInAbyissSMP.aternos.me",
+  port: "38204",
+  username: "Kvinn1",
+  //   password: "Never say Never905",
+  //   username: "jobair.tahsin1@gmail.com",
+  //   password: "Never say Never905",
+};
+
+var bot = mineflayer.createBot(options);
+
+bot.on("spawn", () => {
+  console.log("- Spawned -");
+  console.log("- Listening for messages -");
+  for (const player of Object.values(bot.players)) {
+    console.log("- Player " + player.displayName + " connected -");
+  }
+});
