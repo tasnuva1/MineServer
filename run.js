2f4a74b8ffc7f5ac7e2291a2af66415804482d8c
tasnuva1 <fahmidamashura@gmail.com>
2022-04-26 04:36:29 +0600

    New Version2

--- run.js
+++ run.js
@@ -1,19 +1,23 @@
-const mineflayer = require("mineflayer");
-var options = {
-  host: "MadeInAbyissSMP.aternos.me",
-  port: "38204",
-  username: "Kvinn1",
-  //   password: "Never say Never905",
-  //   username: "jobair.tahsin1@gmail.com",
-  //   password: "Never say Never905",
-};
+const bedrock = require("bedrock-protocol");
+const client = bedrock.createClient({
+  host: "MadeInAbyissSMP.aternos.me", // optional
+  port: 38204, // optional, default 19132
+  username: "Kvinn1", // the username you want to join as, optional if online mode
+  offline: true, // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
+});
 
-var bot = mineflayer.createBot(options);
-
-bot.on("spawn", () => {
-  console.log("- Spawned -");
-  console.log("- Listening for messages -");
-  for (const player of Object.values(bot.players)) {
-    console.log("- Player " + player.displayName + " connected -");
+client.on("text", (packet) => {
+  // Listen for chat messages and echo them back.
+  if (packet.source_name != client.options.username) {
+    client.queue("text", {
+      type: "chat",
+      needs_translation: false,
+      source_name: client.username,
+      xuid: "",
+      platform_chat_id: "",
+      message: `${packet.source_name} said: ${
+        packet.message
+      } on ${new Date().toLocaleString()}`,
+    });
   }
 });
