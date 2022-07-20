2a217f1324e853398a4472919583f726127e6a7a
tasnuva1 <fahmidamashura@gmail.com>
2022-07-20 22:37:20 +0600

    heroku code added

--- run.js
+++ run.js
@@ -1,4 +1,16 @@
 const bedrock = require("bedrock-protocol");
+const throng = require("throng");
+
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
+
+throng(
+  {
+    workers: WORKERS,
+    lifetime: Infinity,
+  },
+  start
+);
+
 const client = bedrock.createClient({
   host: "MadeInAbyissSMP.aternos.me", // optional
   port: 38204, // optional, default 19132
@@ -6,18 +18,20 @@ const client = bedrock.createClient({
   offline: true, // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
 });
 
-client.on("text", (packet) => {
-  // Listen for chat messages and echo them back.
-  if (packet.source_name != client.options.username) {
-    client.queue("text", {
-      type: "chat",
-      needs_translation: false,
-      source_name: client.username,
-      xuid: "",
-      platform_chat_id: "",
-      message: `${packet.source_name} said: ${
-        packet.message
-      } on ${new Date().toLocaleString()}`,
-    });
-  }
-});
+function start() {
+  client.on("text", (packet) => {
+    // Listen for chat messages and echo them back.
+    if (packet.source_name != client.options.username) {
+      client.queue("text", {
+        type: "chat",
+        needs_translation: false,
+        source_name: client.username,
+        xuid: "",
+        platform_chat_id: "",
+        message: `${packet.source_name} said: ${
+          packet.message
+        } on ${new Date().toLocaleString()}`,
+      });
+    }
+  });
+}
